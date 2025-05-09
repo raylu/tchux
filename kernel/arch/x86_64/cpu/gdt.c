03c93f566da3a0a807959db91d4e29c456ddd08a
Francisco Araújo <franciscocoelhoaraujo492000@gmail.com>
2025-05-09 12:27:19 +0100

    add TSS

--- kernel/arch/x86_64/cpu/gdt.c
+++ kernel/arch/x86_64/cpu/gdt.c
@@ -1,12 +1,17 @@
 #include <stdint.h>
 #include <gdt.h>
+#include <memory.h>
 
-struct gdt_entry_struct gdt_entries[6];
+#define KERNEL_STACK_SIZE 8192
+uint8_t kernel_stack[KERNEL_STACK_SIZE];
+
+struct gdt_entry_struct gdt_entries[7];
 struct gdt_ptr_struct gdt_ptr;
+struct tss_entry_struct tss_entry;
 
 void init_gdt()
 {
-	gdt_ptr.limit = sizeof(struct gdt_entry_struct) * 6 - 1;
+	gdt_ptr.limit = sizeof(struct gdt_entry_struct) * 7 - 1;
 	gdt_ptr.base = (uint64_t)&gdt_entries;
 	
 	set_gdt(0,0,0,0,0); // null segment
@@ -14,11 +19,14 @@ void init_gdt()
 	set_gdt(2,0,0,0x92,0xC0); // kernel data
 	set_gdt(3,0,0,0xFA,0xA0); // user code
 	set_gdt(4,0,0,0xF2,0xC0); // user data
+	set_tss(5,(uint64_t)&kernel_stack[KERNEL_STACK_SIZE]);
 	
 	gdt_flush();
+	tss_flush();
 }
 
-void gdt_flush(void) {
+void gdt_flush(void) 
+{
     asm volatile (
         "lgdt %0\n"
         "mov $0x10, %%ax\n"
@@ -45,5 +53,26 @@ void set_gdt(uint64_t id, uint64_t base, uint64_t limit, uint8_t access, uint8_t
 	gdt_entries[id].flags |= flags & 0xF0;
 	
 	gdt_entries[id].access = access;
+}
+
+set_tss(uint64_t id, uint64_t rsp0)
+{
+	uint64_t base = (uint64_t) &tss_entry;
+	uint64_t limit = sizeof(struct tss_entry_struct) - 1;
 	
+	set_gdt(id, base, limit, 0x89, 0x00);
+	
+	memset(&tss_entry, 0, sizeof(tss_entry));	
+	
+	tss_entry.rsp0 = rsp0;
+	tss_entry.iomap = sizeof(tss_entry);
+}
+
+void tss_flush(void)
+{
+	asm volatile (
+		"mov $0x28, %%ax\n"
+		"ltr %%ax\n"
+		: : : "ax"
+	);
 }
--- kernel/arch/x86_64/include/gdt.h
+++ kernel/arch/x86_64/include/gdt.h
@@ -1,7 +1,7 @@
-#include <stdint.h>
-
 #ifndef GDT_H
 #define GDT_H
+
+#include <stdint.h>
 
 struct gdt_entry_struct 
 {
@@ -19,7 +19,27 @@ struct gdt_ptr_struct
 	uint64_t base;
 } __attribute__((packed));
 
+struct tss_entry_struct
+{
+	uint64_t reserved0;
+	uint64_t rsp0;
+	uint64_t rsp1;
+	uint64_t rsp2;
+	uint64_t reserved1;
+	uint64_t ist1;
+	uint64_t ist2;
+	uint64_t ist3;
+	uint64_t ist4;
+	uint64_t ist5;
+	uint64_t ist6;
+	uint64_t ist7;
+	uint64_t reserved2;
+	uint64_t reserved3;
+	uint64_t iomap;
+}__attribute__((packed));
+
 void init_gdt();
 void set_gdt(uint64_t id, uint64_t base, uint64_t limit, uint8_t access, uint8_t flags);
+void set_tss(uint64_t id, uint64_t rsp0);
 
 #endif
