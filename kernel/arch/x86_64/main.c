7c5e4463add284f98746347a808603199e143903
Francisco Araújo <franciscocoelhoaraujo492000@gmail.com>
2025-05-12 15:18:52 +0100

    update main

--- kernel/arch/x86_64/main.c
+++ kernel/arch/x86_64/main.c
@@ -4,20 +4,6 @@
 #include <gdt.h>
 #include <idt.h>
 
-__attribute__((used, section(".limine_requests")))
-static volatile LIMINE_BASE_REVISION(3);
-
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_framebuffer_request framebuffer_request = {
-	.id = LIMINE_FRAMEBUFFER_REQUEST,
-	.revision = 0
-};
-
-__attribute__((used, section(".limine_requests_start")))
-static volatile LIMINE_REQUESTS_START_MARKER;
-
-__attribute__((used, section(".limine_requests_end")))
-static volatile LIMINE_REQUESTS_END_MARKER;
 
 
 static void hcf(void)
@@ -28,14 +14,10 @@ static void hcf(void)
 	}
 }
 
+static volatile LIMINE_BASE_REVISION(3);
 void kmain(void)
 {	
 	if (LIMINE_BASE_REVISION_SUPPORTED == false)
-	{
-		hcf();
-	}
-	
-	if (framebuffer_request.response == NULL || framebuffer_request.response->framebuffer_count < 1)
 	{
 		hcf();
 	}
