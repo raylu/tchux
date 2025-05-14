09784443c8a1c45f9ee4c7fd2a3e47132b24f115
Francisco Araújo <franciscocoelhoaraujo492000@gmail.com>
2025-05-14 13:03:57 +0100

    update framebuffer

--- kernel/include/fb.h
+++ kernel/include/fb.h
@@ -11,5 +11,7 @@ extern uint32_t framebufferPitch;
 extern struct limine_framebuffer limine_fb;
 
 void init_fb();
+void draw_pixel(uint32_t x, uint32_t y, uint32_t color);
+void draw_text(uint32_t x, uint32_t y, const char *text, uint32_t color);
 
 #endif
